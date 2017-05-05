923eae295fe45c879e7ec39f1436494338f87b17
Cedric Roux <cedric.roux22@gmail.com>
2017-05-05 11:29:03 -0400

    Update SQLRequest.java

--- SQLRequest.java
+++ SQLRequest.java
@@ -1,9 +1,14 @@
 package ishare;
-
 import java.sql.*;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.net.*;
-import org.json.*;
+import java.io.*;
 
 public class SQLRequest {
 
@@ -98,24 +103,23 @@ public class SQLRequest {
             ResultSet result = request("SELECT b.code_postal FROM ishare_user user_id = " + user_id );
             result.next();
             String user_postcode = result.getString("code_postal");
-            List<Integer> list = new ArrayList<>;
+            List<Integer> list = new ArrayList<>();
             ResultSet object_result =  request("SELECT objet_id FROM objet ");
             while (result.next()){
 
                 String object_postcode = getObjectLocation(object_result.getInt("objet_id"));
 
                 try {
-                    JSONParser parser = new JSONParser();
                     URL myURL =  new URL("http://maps.googleapis.com/maps/api/distancematrix/json?origins="+ user_postcode +",canada&destinations="+ object_postcode +",canada&mode=driving&language=en-EN&sensor=false");
                     URLConnection myURLConnection = myURL.openConnection();
                     BufferedReader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
-                    String inputLine = in.readLine());
+                    String inputLine = in.readLine();
                     System.out.println(inputLine);
-                    Pattern pattern = Pattern.compile("\d*.?\d* km");
+                    Pattern pattern = Pattern.compile("\\d*.?\\d* km");
                     Matcher matcher = pattern.matcher(inputLine);
-                    int distance_trouver;
+                    String distance_trouver = null;
                     if (matcher.find()){
-                        Pattern pattern2 = Pattern.compile("\d*.?\d*");
+                        Pattern pattern2 = Pattern.compile("\\d*.?\\d*");
                         Matcher matcher2 = pattern2.matcher(matcher.group(1));
                         if (matcher2.find()){
                             distance_trouver = matcher2.group(1);
@@ -133,7 +137,6 @@ public class SQLRequest {
                 catch (IOException e) {
                     e.printStackTrace();
                 }
-                URLConnection myURLConnection = myURL.openConnection();
 
             }
         }catch (SQLException se){
@@ -148,16 +151,21 @@ public class SQLRequest {
 
     public static void createObject(String type, String description, boolean disponible, int prix, int duree_partage, int owner_id){
         try{
+
+            ResultSet result = request("SELECT COUNT(object_id) AS total FROM objet");
+            int current_id = list.add(result.getInt("total"));
+            int next_id = current_id + 1;
+
             Connection con = JavaConnect.connection();
             Statement stmt = con.createStatement();
             stmt.executeUpdate("INSERT INTO objet (objet_id, type, description, disponible, prix, dureeMax, owner_id) Values ("+
-                                                        objet_id+", \'"+
-                                                        type+"\', \'"+
-                                                        description+"\', "+
-                                                        disponible+", "+
-                                                        prix+", "+
-                                                        duree_partage+", "+
-                                                        owner_id");");
+                    next_id+", \'"+
+                    type+"\', \'"+
+                    description+"\', "+
+                    disponible+", "+
+                    prix+", "+
+                    duree_partage+", "+
+                    owner_id + ")");
         }catch (SQLException se){
             se.printStackTrace();
         }catch (Exception e){
@@ -168,14 +176,10 @@ public class SQLRequest {
 
     public static void createUser(String prenom, String nom, String courriel, int numtel, String code_postal, int numero, String rue, String province, String ville){
         try{
-            ResultSet result = request("SELECT COUNT(user_id) AS total FROM ishare_user");
-            List<Integer> list = new ArrayList<>();
-            while (result.next()){
-                list.add(result.getInt("total"));
-            }
+            ResultSet result = request("SELECT COUNT(object_id) AS total FROM objet");
+            int current_id = list.add(result.getInt("total"));
+            int next_id = current_id + 1;
 
-            int next_id = (int)list.get(0) + 1;
-            System.out.println(next_id);
             Connection con = JavaConnect.connection();
             Statement stmt = con.createStatement();
 
@@ -187,25 +191,6 @@ public class SQLRequest {
             e.printStackTrace();
         }
 
-
-    }
-
-    public static void changeObjectDisponibility(int objet_id){
-        try{
-            Connection con = JavaConnect.connection();
-            Statement stmt = con.createStatement();
-            ResultSet result = request("SELECT disponible FROM objet WHERE objet_id = "+objet_id);
-            result.next();
-            if (result.getInt("disponible") == 1){
-                stmt.executeUpdate("UPDATE objet SET disponible = 0");
-            }else{
-                stmt.executeUpdate("UPDATE objet SET disponible = 1");
-            }
-        }catch (SQLException se){
-            se.printStackTrace();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 
     public static void changeObjectPrice(int object_id,int price){
@@ -282,26 +267,63 @@ public class SQLRequest {
         return null;
     }
 
-    public static List<String> getObjectInfo(int objet_id){
-        try{
-            ResultSet result = request("SELECT * FROM objet WHERE objet_id = " + user_id);
-            List<String> list= new ArrayList<>();
+    public static List<String> getObjetInfo(int objet_id){
 
-            result.next()
-            list.add(result.getString("type"));
-            list.add(result.getString("disponible"));
-            list.add(result.getString("prix"));
-            list.add(result.getString("dureeMax"));
+        try{
+            ResultSet result = request("SELECT type FROM (SELECT * FROM objet WHERE objet_id ="+objet_id+")");
+            String my_type= "";
+            result.next();
+            my_type = my_type + (result.getString("type"));
+            ResultSet result_end = request("SELECT * FROM objet NATURAL JOIN "+ my_type + " WHERE objet_id ="+objet_id);
+            List<String> list = new ArrayList<>();
+            result_end.next();
+            list.add(""+result_end.getInt("objet_id"));
+            list.add(result_end.getString("type"));
+            list.add(result_end.getString("description"));
+            list.add(""+result_end.getInt("disponible"));
+            list.add(""+result_end.getInt("prix"));
+            list.add(""+result_end.getInt("dureemax"));
+            list.add(""+result_end.getInt("owner_id"));
+
+            if(my_type.equals("auto")){
+                list.add(result_end.getString("marque"));
+                list.add(result_end.getString("modele"));
+                list.add(result_end.getString("couleur"));
+                list.add(""+result_end.getInt("annee"));
+                list.add(""+result_end.getInt("transmission_manuel"));
+                list.add(""+result_end.getInt("nbPortes"));
+                list.add(""+result_end.getInt("consommation"));
+            }else if (my_type.equals("maison")){
+                list.add(""+result_end.getInt("annee"));
+                list.add(""+result_end.getInt("superficie"));
+                list.add(""+result_end.getInt("etage"));
+                list.add(""+result_end.getInt("chambre"));
+                list.add(""+result_end.getInt("meuble"));
+                list.add(""+result_end.getInt("stationnement"));
+                list.add(""+result_end.getInt("salle_bain"));
+            }else{
+                list.add(result_end.getString("type_velo"));
+                list.add(result_end.getString("marque"));
+                list.add(result_end.getString("modele"));
+                list.add(result_end.getString("cadre"));
+                list.add(result_end.getString("freins"));
+                list.add(result_end.getString("taille_pneus"));
+                list.add(""+result_end.getInt("annee"));
+                list.add(result_end.getString("sexe"));
+            }
 
             return list;
+
         }catch (SQLException se){
             se.printStackTrace();
         }catch (Exception e){
             e.printStackTrace();
         }
         return null;
-    }
 
+
+
+    }
 
 
     public static List<String> getUserInfo(int user_id){
@@ -309,7 +331,7 @@ public class SQLRequest {
             ResultSet result = request("SELECT * FROM ishare_user WHERE user_id = " + user_id);
             List<String> list= new ArrayList<>();
 
-            result.next()
+            result.next();
             list.add(result.getString("prenom"));
             list.add(result.getString("nom"));
             list.add(result.getString("courriel"));
@@ -334,9 +356,9 @@ public class SQLRequest {
             Connection con = JavaConnect.connection();
             Statement stmt = con.createStatement();
             DateFormat dateFormat = new SimpleDateFormat("DD/MM/YYYY");
-            Date date = new Date();
-            stmt.executeUpdate("INSERT INTO emprunte(user_id, objet_id, duree, date_in, date_out) Values (" + user_id + ", " + object_id + ", " + duree + ", TO_DATE(' "+ date +"', 'DD/MM/YYYY'), NULL); ");
-            changeObjectDisponibility(object_id)
+            Calendar cal = Calendar.getInstance();
+            stmt.executeUpdate("INSERT INTO emprunte(user_id, objet_id, duree, date_in, date_out) Values (" + user_id + ", " + object_id + ", " + duree + ", TO_DATE(' "+ cal +"', 'DD/MM/YYYY'), NULL); ");
+            changeObjectDisponibility(object_id);
         }catch (SQLException se){
             se.printStackTrace();
         }catch (Exception e){
@@ -349,8 +371,9 @@ public class SQLRequest {
             Connection con = JavaConnect.connection();
             Statement stmt = con.createStatement();
             DateFormat dateFormat = new SimpleDateFormat("DD/MM/YYYY");
-            Date date = new Date();
-            stmt.executeUpdate("UPDATE emprunt SET date_out = TO_DATE(' "+ date +"' WHERE user_id = " + user_id + " AND objet_id = " + object_id);
+            Calendar cal = Calendar.getInstance();
+
+            stmt.executeUpdate("UPDATE emprunt SET date_out = TO_DATE(' "+ cal +"' WHERE user_id = " + user_id + " AND objet_id = " + object_id);
         }catch (SQLException se){
             se.printStackTrace();
         }catch (Exception e){
